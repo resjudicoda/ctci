3e5b40d6baa70508985e3386fa628300204f8e29
resjudicoda <adam.j.reis@gmail.com>
2022-04-06 15:36:03 -0700

    readme and initial changes

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # ctci
+
+This is my personal repository for working through CTCI in Javascript.
+
+The solutions here are all or mostly mine. I try to include comments in solutions if I looked to code from an outside source to help me solve the problem. The tests are written in Jasmine, and they are all mine. I build them out as I solve the problems.
+
+To help with the repetition of coding and testing, I wrote a custom shell command that takes one argument from the shell, creates a directory with that argument name, moves into that directory, creates template .js and spec.js files (again with the argument name), and then opens those files in VSCode.
+
+For example, running "problem is_unique" from the shell creates a directory called "is_unique." The new directory will have two files called "is_unique.js" and "is_unique.spec.js", each populated with some working template code. Running "jasmine is_unique.spec.js" will run the tests (assumign you have Jasmine installed). Changing the function in your .js file to return 'true' will get the test in the .spec.js file passing. From there, its up to you.
+
+The shell command function is:
+
+function problem() {
+mkdir $1
+cd $1
+touch $1.js
+cat <<eos > $1.js
+const $1 = function() {
+return false;
+}
+module.exports = $1
+eos
+touch $1.spec.js
+cat <<eos > $1.spec.js
+const $1 = require('./$1')
+describe('$1', function() {
+it('works', function() {
+expect($1('test')).toEqual(true);
+});
+});
+eos
+code .
+}
+
+Here is a tutorial on how to create custom shell commands if you need it (I sure did). https://medium.com/devnetwork/how-to-create-your-own-custom-terminal-commands-c5008782a78e
--- arrays_and_strings/rotate_matrix/rotate_matrix.js
+++ arrays_and_strings/rotate_matrix/rotate_matrix.js
@@ -2,58 +2,7 @@ const rotate_matrix = function(matrix) {
   if (!matrix) return false;
   let n = matrix.length;
   if (n == 0 || n != matrix[0].length) return false;
-  // ctci implementations
-  // for (let layer = 0; layer < n/2; layer++) {
-  //   let first = layer;
-  //   let last = n - 1 - layer;
-  //   for (let i = first; i < last; i++) {
-  //     let offset = i - first;
-  //     //store top
-  //     let top = matrix[first][i];
-  //     //left -> top
-  //     matrix[first][i] = matrix[last - offset][first];
-  //     //bottom -> left
-  //     matrix[last - offset][first] = matrix[last][last - offset];
-  //     //right -> bottom
-  //     matrix[last][last - offset] = matrix[i][last];
-  //     //top -> right
-  //     matrix[i][last] = top;
-  //   }
-  // }
 
-  // 123  741
-  // 456  852
-  // 789  963
-
-  // rows become columns in reverse order
-  // columns become rows in same order but reverse sorted
-  // rebuild it started with first row, but start at last column
-  // 1: row[0][0] -> row[0][2] 
-  // 2: row[0][1] -> row[1][2] 
-  // 3: row[0][2] -> row[2][2] 
-
-  // 4: row[1][0] -> row[0][1]
-  // 5: row[1][1] -> row[1][1]
-  // 6: row[1][2] -> row[2][1]
-
-  // 7: row[2][0] -> row[0][0]
-  // 8: row[2][1] -> row[1][0]
-  // 9: row[2][2] -> row[2][0]
-
-  // variables
-  // outerloop 0 - 2
-  // innerloop 0 - 2
-  // outerloop 2 - 0
-
-  // row number becomes your reverse array position
-  // array position becomes your row number
-
-  // i think this will only work with a copy of the matrix
-  // while rows become columns, once you make one change, you dont have the same rows and columns
-
-  // got some help from leetcode
-  // i has to go to (n + 1) / 2
-  // j has to go to n / 2
   for (let i = 0; i < Math.floor((n + 1) / 2); i++) {
     for (let j = 0; j < Math.floor(n / 2); j++) {
       // need
@@ -75,12 +24,6 @@ const rotate_matrix = function(matrix) {
       matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
       matrix[j][n - 1 - i] = matrix[i][j];
       matrix[i][j] = temp;
-
-
-        // desired
-          // 123  741
-          // 456  852
-          // 789  963
     }
   }
   console.log('matrix', matrix)
