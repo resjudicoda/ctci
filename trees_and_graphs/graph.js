57ec4b9df2d9cdf4fc682f03aaa6d64f5522d2d3
resjudicoda <adam.j.reis@gmail.com>
2022-04-18 09:47:23 -0700

    basic graph class working

--- trees_and_graphs/graph.js
+++ trees_and_graphs/graph.js
@@ -5,23 +5,17 @@ class Graph {
     }
   
     add(value, children) {
-      // your code here
+      // give new node an id equivalent to this.nodes.length + 1
       let numberOfNodes;
       if (this.nodes.length) {
         numberOfNodes = this.nodes.length
       } else numberOfNodes = 0
-      
       const id = numberOfNodes + 1;
-      // need to turn children ids into actual nodes
-      // consider comparing to this.nodes for matching ids
-      const newChildren = [];
-      console.log('children', children)
-      // need to get this working
+      // compare children to this.nodes ids to put actual nodes into children array
+      let newChildren = [];
       if (children && children.length) {
         children.forEach((child) => {
-            newChildren.push(this.nodes.filter((node) => {
-                node.id == child;
-            }))
+            newChildren.push(this.nodes.filter(node => node.id == child)[0])
         })
       }
       const newNode = new Node(id, value, newChildren);
--- trees_and_graphs/graph.spec.js
+++ trees_and_graphs/graph.spec.js
@@ -22,7 +22,27 @@ describe('A Graph', function() {
         expect(graph.nodes[0].id).toBe(1);
         expect(graph.nodes[1].id).toBe(2);
         expect(graph.nodes[0].children).toEqual([]);
-        expect(graph.nodes[1].children).toBe(false);
+        expect(graph.nodes[1].children.length).toBe(1);
+    })
+
+    it('can add many nodes', function () {
+        graph.add(1)
+        .add(2, [1])
+        .add(3, [1, 2])
+        .add(4, [2])
+        .add(5, [1])
+        .add(6, [1, 2, 3, 4, 5])
+        expect(graph.nodes.length).toBe(6);
+        expect(graph.nodes[0].id).toBe(1);
+        expect(graph.nodes[1].id).toBe(2);
+        expect(graph.nodes[2].id).toBe(3);
+        expect(graph.nodes[3].id).toBe(4);
+        expect(graph.nodes[4].id).toBe(5);
+        expect(graph.nodes[5].id).toBe(6);
+        expect(graph.nodes[0].children).toEqual([]);
+        expect(graph.nodes[1].children.length).toBe(1);
+        expect(graph.nodes[2].children.length).toBe(2);
+        expect(graph.nodes[5].children.length).toBe(5);
     })
 
 })
